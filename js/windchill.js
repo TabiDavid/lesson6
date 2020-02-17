3d9555d5d51f4a8f7b37ec13d31756079636e8a2
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2020-02-16 20:21:27 -0500

    commit

--- js/windchill.js
+++ js/windchill.js
@@ -4,5 +4,5 @@ if(temp <= 50 && windspeed >= 3) {
     let mathstuff = 35.74 + (0.6215*temp) - (35.75*(windspeed**0.16)) + (0.4275*temp*(windspeed**0.16));
     document.getElementById("windchill").innerText = mathstuff.toFixed(2);
 } else {
-   document.getElementById("windchill").innerText = "N/A" 
+   document.getElementById("windchill").innerText = "N/A"; 
 }
