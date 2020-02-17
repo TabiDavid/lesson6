4348ddc1a586a0bfc56f88e1f16a782b5d46d5cd
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2020-02-16 20:54:18 -0500

    commit

--- js/windchill.js
+++ js/windchill.js
@@ -1,17 +1,9 @@
 
-function windChill() {
-    let windSpeed = document.getElementById('Wspeed').innerHTML;
-    windSpeed = parseFloat(windSpeed);
-    let temperature = document.getElementById('temperature').innerHTML;
-    temperature = parseFloat(temperature);
-    if (temperature <= 50 && windSpeed >= 3) {
-        let windspeedExponent = Math.pow(windSpeed, 0.16);
-        let windChill = (35.74 + 0.6215 * temperature - 35.75 * windspeedExponent + 0.4275 * temperature * windspeedExponent).toFixed(0);
-        return windChill;
-    } else {
-        return temperature;
-    }
-
+let temp = parseInt(document.getElementById("temperature"));
+let windspeed = parseInt(document.getElementById("windspeed"));
+if(temp <= 50 && windspeed >= 3) {
+    let mathstuff = 35.74 + (0.6215*temp) - (35.75*(windspeed**0.16)) + (0.4275*temp*(windspeed**0.16));
+    document.getElementById("windchill").innerText = mathstuff.toFixed(2);
+} else {
+   document.getElementById("windchill").innerHTML = "N/A"; 
 }
-
-document.getElementById("Windchill").innerHTML = windChill() + "&#xb0;F";
